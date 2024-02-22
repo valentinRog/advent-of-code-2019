e8e869d124294776cb0ff760cca2877a8d1a0f98
Valentin Rogister <valentin.rogister@gmail.com>
2024-02-22 01:42:06 +0100

    day01

---
+++ day01/part2.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
+    const alloc = std.heap.page_allocator;
+    const ws = "\n\r ";
+    const data = try stdin.readAllAlloc(alloc, 1 << 16);
+    var it = std.mem.split(u8, std.mem.trim(u8, data, ws), "\n");
+    var res: i32 = 0;
+    while (it.next()) |s| {
+        var n: i32 = try std.fmt.parseInt(i32, std.mem.trim(u8, s, ws), 10);
+        n = @divFloor(n, 3) - 2;
+        while (n > 0) : (n = @divFloor(n, 3) - 2) {
+            res += n;
+        }
+    }
+    try std.io.getStdOut().writer().print("{}\n", .{res});
+}
